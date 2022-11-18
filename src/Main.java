5056a82d3ce00dec055fbbcb3fa3aacbc5ecdf29
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-18 10:06:44 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Keywords and Expresssions");
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344); //sample expression
+        int highScore = 50;   //valid expression
+        if (highScore == 50){ //valid expression
+            System.out.printf("This is an expression");
+        }
+
     }
 }
